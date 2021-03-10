f9e8c246dd5279605e3a530ae81c5fed4bd3f5cb
Steven De Ryck <steven@inform.be>
2021-03-10 23:20:39 +0100

    CORS test - Authentication

--- server/1_get_text/endpoint_text.php
+++ server/1_get_text/endpoint_text.php
@@ -1,7 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+//header("Access-Control-Allow-Origin: https://www.example.com");
 
 //hardcoded array
-$variable1= "Hello World";
+$variable1= "Hello World, en ik kom van de server";
 
 //natuurlijk kan je ook data uit de databank halen
 
--- server/1_get_text/restclient_simple.js
+++ server/1_get_text/restclient_simple.js
@@ -1,5 +1,6 @@
 
 var url = 'endpoint_text.php';
+//var url = 'https://wdev2.be/steven21/rest/server/1_get_text/endpoint_text.php';
 
 fetch(url)
     .then(function(response) {
